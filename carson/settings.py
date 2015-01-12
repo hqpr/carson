33641a08bfc193049593b0bfce5d5d672f85367b
Sergey <kartoswko@gmail.com>
2015-01-12 10:52:32 +0100

    HEROKU

--- carson/settings.py
+++ carson/settings.py
@@ -97,8 +97,9 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'staticfiles'),
 )
+
 
 # List of finder classes that know how to find static files in
 # various locations.
