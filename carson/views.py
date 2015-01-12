6b2aa7d701c792c09e96386e86ba405112932283
Sergey <kartoswko@gmail.com>
2015-01-12 18:17:21 +0100

    Update views.py

--- carson/views.py
+++ carson/views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.models import User
 from django.shortcuts import render
 
 from .models import Products, Slides, ImgBlocks, Category, SubCategory
@@ -18,8 +17,3 @@ def product(request, id):
     product = Products.objects.get(id=id)
     data = {'product': product}
     return render(request, 'product.html', data)
-    
-    
-def create_superuser(request):
-    User.objects.create_superuser('admin1', 'adubnyak@gmail.com', 1)
-    return None
